e7a93a7978e9266b3bffe3804b6bf50dcfd93432
RedRiot <quoc.tung@outlook.com>
2018-07-25 10:28:45 +0200

    add() method

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        int number = 2;
-        int number1 =3;
 
-        sumTwoNumbers (number,number1);
     }
 
-    private static int sumTwoNumbers(int number, int number2) {
-        int sum = number + number2;
-        return sum;
+    private static void greet(String word) {
+        System.out.println(word);
     }
 
 
