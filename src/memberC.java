d20a4b3e4cd80ca1c4df7e7aa4fcf1ec6c146b19
ebenyu08 <egri.benyu@gmail.com>
2018-07-25 10:48:06 +0200

    c5

--- src/memberC.java
+++ src/memberC.java
@@ -1,6 +1,6 @@
 public class memberC {
-    public static void cheer(String param) {
+    public static void cheer(String param1, String param2) {
         //System.out.printf(param);
-        System.out.println("java");
+        System.out.println(param1 + " " + param2);
     }
 }
